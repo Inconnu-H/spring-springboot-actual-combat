57bed9e829df9edbb3fe6b8ac87e215bd3d77e0d
Ping <Inconnu.H@hotmail.com>
2020-03-02 23:29:22 +0800

    Bean的Scope示例练习

---
+++ src/main/java/com/ping/spring/springboot/actualcombat/chapter2/scope/ScopeConfig.java
@@ -0,0 +1,9 @@
+package com.ping.spring.springboot.actualcombat.chapter2.scope;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan("com.ping.spring.springboot.actualcombat.chapter2.scope")
+public class ScopeConfig {
+}
