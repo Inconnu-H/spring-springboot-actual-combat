0f9add2dfd9fa5660c8359d526d4b08a769be25f
Ping <Inconnu.H@hotmail.com>
2020-03-29 14:10:29 +0800

    2.4 Profile

---
+++ src/main/java/com/ping/spring/springboot/actualcombat/chapter2/event/DemoListener.java
@@ -0,0 +1,17 @@
+package com.ping.spring.springboot.actualcombat.chapter2.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoListener implements ApplicationListener<DemoEvent> {
+
+
+    @Override
+    public void onApplicationEvent(DemoEvent demoEvent) {
+        String msg = demoEvent.getMsg();
+        System.out.println("我(bean-demoListener)接收到了bean-demoPublisher发布的消息: "  + msg);
+    }
+}
